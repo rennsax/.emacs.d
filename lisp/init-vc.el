c686e91b121c9312a93196c4eaa8bc43b2c89ee0
Bojun Ren <bj.ren.coding@outlook.com>
2024-04-20 10:42:47 +0800

    feat(magit): port doom's magit configs
    Add magit autoload file copied from doom's.
    
    Config magit.

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -6,23 +6,57 @@
 (eval-when-compile
   (require 'init-const))
 
+;; There is also a builtin `transient' package.
+;; However we use the submodule one.
+(use-package transient
+  :load-path "packages/transient/lisp"
+  :init
+  ;; Set transient directories.
+  (setq transient-levels-file  (concat celeste-data-dir "transient/levels")
+        transient-values-file  (concat celeste-data-dir "transient/values")
+        transient-history-file (concat celeste-data-dir "transient/history"))
+  :config
+  (setq transient-default-level 5
+        ;; Always display the transient popup buffer below.
+        transient-display-buffer-action '(display-buffer-below-selected))
+  (keymap-set transient-map "<escape>" #'transient-quit-one))
+
 ;; TODO: `magit-submodule-add-1' git submodule absortgitdirs
 (celeste/use-package magit
-  :defines magit-mode-map
+  :load-path "packages/magit/lisp"
+  :demand t ; The killer feature of Emacs! Load it immediately!
   :init
-  ;;; Add deps
-  ;; compat
-  ;; dash
-  ;; There is also a builtin `transient' package.
-  ;; However we use the submodule one.
-  (celeste/use-package transient
-    :load-path "packages/transient/lisp")
+  ;; Also depends on `compat' and `dash'.
   (celeste/use-package with-editor
     :load-path "packages/with-editor/lisp")
-  :load-path "packages/magit/lisp"
-  :demand t
   :config
+  (setq magit-diff-refine-hunk t ; show granular diffs in selected hunk
+        ;; Just trust the user, instead of saving files before running magit
+        ;; commands.
+        magit-save-repository-buffers nil)
+
   (keymap-set magit-mode-map "s-r" 'magit-refresh)
+  (keymap-unset magit-mode-map celeste-leader-key)
+
+  ;; Show gravatars when viewing revision.
+  (setq magit-revision-show-gravatars '("^Author:     " . "^Commit:     "))
+
+  ;; Magit window settings.
+  (celeste/autoload '+magit-display-buffer-fn magit)
+  (setq magit-bury-buffer-function #'magit-mode-quit-window
+        magit-display-buffer-function #'+magit-display-buffer-fn)
+
+  ;; Add missing options.
+  (transient-append-suffix 'magit-pull "-r"
+    '("-a" "Autostash" "--autostash"))
+
+  ;; Clean up after magit by killing leftover magit buffers and reverting
+  ;; affected buffers (or at least marking them as need-to-be-reverted).
+  (celeste/autoload '+magit/quit magit)
+  (celeste/autoload '+magit/quit-all magit)
+  (define-key magit-mode-map "q" #'+magit/quit)
+  (define-key magit-mode-map "Q" #'+magit/quit-all)
+
   )
 
 ;; diff-hl: better git-diff integration
