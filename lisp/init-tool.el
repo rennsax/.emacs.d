3566fb697c92e9afe2f6a02ddbeaeb6574411fe0
Bojun Ren <bj.ren.coding@outlook.com>
2024-04-21 00:58:12 +0800

    feat(tool): add perspective.el
    Perspective is a brilliant tool to manage Emacs *workspaces*. It also provides
    the functionality to persist the window layout.

--- lisp/init-tool.el
+++ lisp/init-tool.el
@@ -19,6 +19,26 @@
     (kbd "C-k") #'deadgrep-backward-filename
     (kbd "C-j") #'deadgrep-forward-filename))
 
+;; Powerful and convenient *workspace* manager.
+(celeste/use-package perspective
+  ;; Differences: these two commands respect `ido-ignore-buffers'.
+  :bind (("C-x b" . persp-switch-to-buffer*)
+         ("C-x k" . persp-kill-buffer*))
+  :hook
+  ((after-init . persp-mode)
+   (kill-emacs . persp-state-save))
+  :config
+  ;; I don't set `persp-mode-prefix-key' - I manually map `perspective-map'.
+  ;; That's because I'm using EVIL 😈.
+  (setq persp-suppress-no-prefix-key-warning t)
+  ;; Ignore some buffers with `persp-switch-to-buffer*'
+  (setq ido-ignore-buffers `("\\` " "\\*helpful command: .*\\*"))
+  (setq persp-state-default-file (concat celeste-data-dir "last-perspective"))
+
+  (evil-define-key* nil celeste-leader-map "p" 'perspective-map)
+  (keymap-set global-map "C-x x" 'perspective-map)
+  )
+
 
 (provide 'init-tool)
 ;;; init-tool.el ends here
