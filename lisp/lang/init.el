73b4732c445151c72a9b60e83a2c9edeae904798
Bojun Ren <bj.ren.coding@outlook.com>
2024-04-21 21:51:38 +0800

    feat(treesit): basic treesit configs with `treesit-auto'
    Emacs 29.1 has shipped with the treesit package. The command
    `treesit-install-language-grammar' find recipes from the variable
    `treesit-language-source-alist', which is originally nil. The convenient package
    treesit-auto maintains a list of recipes and appends rules to
    `major-mode-remap-alist', tremendously easing the trouble to configure treesit
    in Emacs.
    
    I prefer to install *all* tree-sitter parsers manually, so the behavior is
    predictable (and I do not want to enable treesit *everywhere* since it may be
    unstable). So I write me a function `+treesit-auto-install' to help with the
    stuff.

--- lisp/lang/init.el
+++ lisp/lang/init.el
@@ -83,6 +83,44 @@ immediately."
           (cdar (exec-path-from-shell-copy-envs (list variable))))))
   )
 
+(use-package treesit
+  :config
+  ;; Level = 4: the highest level, which fontifies almost everything with TS.
+  (setq treesit-font-lock-level 4))
+
+(celeste/use-package treesit-auto
+  :hook (after-init . global-treesit-auto-mode)
+  :config
+  ;; NOTE: Cpp parser is based on C parser. Remember to install them simultaneously.
+
+  ;; Initialize `treesit-language-source-alist'. By default it's nil in Emacs.
+  (setq treesit-language-source-alist
+        (treesit-auto--build-treesit-source-alist))
+  ;; I install grammar manually.
+  (setq treesit-auto-install nil)
+
+  (defun +treesit-auto-install (lang &optional ensure)
+    "Install LANG grammar. LANG is a symbol of the language to be installed.
+
+If ENSURE is non-nil, do nothing if the grammar for LANG has been installed."
+    (interactive (list (intern
+                        (completing-read
+                         "Language: "
+                         (mapcar #'car treesit-language-source-alist)))))
+    (let* ((recipe (seq-filter (lambda (recipe) (eq (car recipe) lang)) treesit-language-source-alist))
+           (installed (treesit-ready-p lang t))
+           (skip (and installed ensure)))
+      (if skip t
+        (if recipe
+            (when (and (or (not installed)
+                           (y-or-n-p (format "Grammar for %s has been installed. Reinstall it?" lang)))
+                       (y-or-n-p (format "Install grammar for %s?" lang)))
+              (treesit-install-language-grammar lang))
+          (progn (message "No recipe for %s. Make sure you did not modify `treesit-auto-langs'." lang)
+                 nil)))))
+  )
+
+
 (require 'init-yaml)
 (require 'init-go)
 (require 'init-markdown)
