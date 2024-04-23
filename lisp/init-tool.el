445f0e1f9829ac9890f44fbf40d52352c669a36e
Bojun Ren <bj.ren.coding@outlook.com>
2024-04-23 09:29:05 +0800

    feat(tool): add vterm, better terminal emulator in Emacs
    Emacs with dynamic modules supported can leverage libvterm to emulate the
    terminal. It's relatively fast and stable.

--- lisp/init-tool.el
+++ lisp/init-tool.el
@@ -71,6 +71,28 @@
         jinx-enable-mode-list)
   )
 
+;; In-Emacs terminal emulator.
+(celeste/use-package vterm
+  ;; TODO: tell whether current Emacs support dynamic-modules.
+  :when (bound-and-true-p module-file-suffix)
+  :commands vterm
+  :config
+  ;; HACK Because vterm clusmily forces vterm-module.so's compilation on us when
+  ;;      the package is loaded, this is necessary to prevent it when
+  ;;      byte-compiling this file (`use-package' blocks eagerly loads packages
+  ;;      when compiled).
+  (when noninteractive
+    (advice-add #'vterm-module-compile :override #'ignore)
+    (provide 'vterm-module))
+
+  ;; Kill the vterm immediately once vterm is dead.
+  (setq vterm-kill-buffer-on-exit t)
+
+  ;; Add a zero after the default value.
+  (setq vterm-max-scrollback 10000)
+
+  ;; The horizontal margin is useless.
+  (add-hook 'vterm-mode-hook #'(lambda () (setq-local hscroll-margin 0))))
 
 
 (provide 'init-tool)
