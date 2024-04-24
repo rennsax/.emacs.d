94851774635a51195af19401b4f12ca482176659
Bojun Ren <bj.ren.coding@outlook.com>
2024-04-24 16:54:32 +0800

    feat(tool): add projectile
    Basic configs are included. Waiting for bindings.

--- lisp/init-tool.el
+++ lisp/init-tool.el
@@ -98,6 +98,37 @@
                                  (when (featurep 'evil-escape)
                                    (evil-escape-mode -1)))))
 
+(celeste/use-package projectile
+  ;; Global minor mode to enable projectile functionalities.
+  :hook (after-init . projectile-mode)
+  :init
+  (setq projectile-cache-file (concat celeste-cache-dir "projectile.cache")
+        projectile-known-projects-file (concat celeste-data-dir "projectile-bookmarks.eld"))
+  :commands projectile-dired
+  :autoload project-projectile
+  :config
+  ;; Integration with project.el: use projectile's use Projectile’s project
+  ;; lookup function (projectile-project-root) and project file lookup function
+  ;; (projectile-project-files) whenever projectile-mode is enabled. Why?
+  ;; Because many packages (eglot, deadgrep, etc.) support natively only
+  ;; project.el's APIs.
+  (add-hook 'project-find-functions #'project-projectile)
+  (setq projectile-sort-order 'recentf
+        projectile-indexing-method 'alien
+        ;; If the cache is stale enough, clean with `projectile-invalidate-cache'.
+        projectile-enable-caching t
+        ;; After 10 minutes, the remote file existence cache expires.
+        projectile-file-exists-remote-cache-expire (* 10 60)
+        ;; Use # as the comment prefix for dirconfig (".projectile" by default).
+        projectile-dirconfig-comment-prefix "#"
+        ;; Run dired after switch to a project.
+        projectile-switch-project-action #'projectile-dired
+        ;; Keep the current project when switching projects, but put it at the end.
+        projectile-current-project-on-switch 'move-to-end)
+  )
+
+
+
 
 (provide 'init-tool)
 ;;; init-tool.el ends here
