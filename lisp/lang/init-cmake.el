7021ef2d6ebe6f6324ef995e9aaf1b64bc25a63b
Bojun Ren <bj.ren.coding@outlook.com>
2024-04-27 16:27:01 +0800

    feat(cmake): add cmake support
    cmake-mode.el is provided by the CMake official repo.

---
+++ lisp/lang/init-cmake.el
@@ -0,0 +1,8 @@
+;;; init-cmake.el -- CMake support. -*- lexical-binding: t; no-byte-compile: t; -*-
+;;; Commentary:
+;;; Code:
+
+(celeste/require cmake-mode)
+
+(provide 'init-cmake)
+;;; init-cmake.el ends here
