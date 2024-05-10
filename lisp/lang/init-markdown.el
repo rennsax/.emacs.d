f68a690807bea640d000d710838c3daa8a6ae19f
Bojun Ren <bj.ren.coding@outlook.com>
2024-05-10 11:15:54 +0800

    feat(md): update configs

--- lisp/lang/init-markdown.el
+++ lisp/lang/init-markdown.el
@@ -8,6 +8,10 @@
   :mode (("\\.md\\'" . markdown-mode)
          ;; Github Flavoured Markdown files
          ("README\\.md\\'" . gfm-mode))
+  ;; Customized manually in UI.
+  ; :custom
+  ; ((markdown-header-scaling t)          ; Use variable-height faces for headers.
+  ;  (markdown-header-scaling-values '(1.6 1.4 1.2 1.1 1.0 1.0)))
   :config
   ;; Basic settings.
   (setq markdown-italic-underscore t ; Prefer underscores over asterisks when
@@ -17,7 +21,17 @@
         ;; `gfm-mode' checkboxs are truned into buttons.
         markdown-make-gfm-checkboxes-buttons t
         ;; Fontify code in code blocks using the native major mode.
-        markdown-fontify-code-blocks-natively t)
+        markdown-fontify-code-blocks-natively t
+
+        ;; Put # in the left margin
+        ;; BUG: conflict with `olivetti-mode'
+        ; markdown-marginalize-headers t
+        ; markdown-marginalize-headers-margin-width 4
+
+        markdown-url-compose-char 8230  ; dots
+        )
+
+  (setq-default markdown-hide-urls t)
 
   ;; Dependency for editing code blocks in markdown-mode.
   (celeste/use-package edit-indirect)
