a9d3fdb3a88cbda4af48e46aa442c1ca2998f47b
Bojun Ren <bj.ren.coding@outlook.com>
2024-05-18 11:28:20 +0800

    feat(treasure): vertico configs refactor and complementary

--- lisp/init-treasure.el
+++ lisp/init-treasure.el
@@ -37,52 +37,55 @@
 (use-package vertico
   :hook ((after-init . vertico-mode))
   :config
-  ;; DEL and M-DEL will delete a part of the path (divided by /) when possible.
-  (use-package vertico-directory
-    :hook (rfn-eshadow-update-overlay . vertico-directory-tidy) ; TODO: what's `rfn-eshadow-update-overlay'?
-    :bind (:map vertico-map
-                ("DEL" . vertico-directory-delete-char)
-                ("M-DEL" . vertico-directory-delete-word)))
+  ;; Cycle candidates.
+  (setq vertico-cycle t))
 
-  (use-package vertico-multiform
-    :hook (vertico-mode . vertico-multiform-mode)
-    :config
+;; DEL and M-DEL will delete a part of the path (divided by /) when possible.
+(use-package vertico-directory
+  :after rfn-eshadow
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy)
+  :bind (:map vertico-map
+              ("DEL" . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word)))
 
-    ;; From https://github.com/minad/vertico/wiki#candidate-display-transformations-custom-candidate-highlighting
-    ;; Show different colors for directories/enabled modes in vertico.
-    (defvar +vertico-transform-functions nil)
+(use-package vertico-multiform
+  :hook (vertico-mode . vertico-multiform-mode)
+  :config
 
-    (cl-defmethod vertico--format-candidate :around
-      (cand prefix suffix index start &context ((not +vertico-transform-functions) null))
-      (dolist (fun (ensure-list +vertico-transform-functions))
-        (setq cand (funcall fun cand)))
-      (cl-call-next-method cand prefix suffix index start))
+  ;; From https://github.com/minad/vertico/wiki#candidate-display-transformations-custom-candidate-highlighting
+  ;; Show different colors for directories/enabled modes in vertico.
+  (defvar +vertico-transform-functions nil)
 
-    (defun +vertico-highlight-directory (file)
-      "If FILE ends with a slash, highlight it as a directory."
-      (when (string-suffix-p "/" file)
-        (add-face-text-property 0 (length file) 'marginalia-file-priv-dir 'append file))
-      file)
+  (cl-defmethod vertico--format-candidate :around
+    (cand prefix suffix index start &context ((not +vertico-transform-functions) null))
+    (dolist (fun (ensure-list +vertico-transform-functions))
+      (setq cand (funcall fun cand)))
+    (cl-call-next-method cand prefix suffix index start))
 
-    (defun +vertico-highlight-enabled-mode (cmd)
-      "If MODE is enabled, highlight it as font-lock-constant-face."
-      (let ((sym (intern cmd)))
-        (with-current-buffer (nth 1 (buffer-list))
-          (if (or (eq sym major-mode)
-                  (and
-                   (memq sym minor-mode-list)
-                   (boundp sym)
-                   (symbol-value sym)))
-              (add-face-text-property 0 (length cmd) 'font-lock-constant-face 'append cmd)))
-        cmd))
+  (defun +vertico-highlight-directory (file)
+    "If FILE ends with a slash, highlight it as a directory."
+    (when (string-suffix-p "/" file)
+      (add-face-text-property 0 (length file) 'marginalia-file-priv-dir 'append file))
+    file)
 
-    (add-to-list 'vertico-multiform-categories
-                 '(file
-                   (+vertico-transform-functions . +vertico-highlight-directory)))
-    (add-to-list 'vertico-multiform-commands
-                 '(execute-extended-command
-                   (+vertico-transform-functions . +vertico-highlight-enabled-mode))))
-  )
+  (defun +vertico-highlight-enabled-mode (cmd)
+    "If MODE is enabled, highlight it as font-lock-constant-face."
+    (let ((sym (intern cmd)))
+      (with-current-buffer (nth 1 (buffer-list))
+        (if (or (eq sym major-mode)
+                (and
+                 (memq sym minor-mode-list)
+                 (boundp sym)
+                 (symbol-value sym)))
+            (add-face-text-property 0 (length cmd) 'font-lock-constant-face 'append cmd)))
+      cmd))
+
+  (add-to-list 'vertico-multiform-categories
+               '(file
+                 (+vertico-transform-functions . +vertico-highlight-directory)))
+  (add-to-list 'vertico-multiform-commands
+               '(execute-extended-command
+                 (+vertico-transform-functions . +vertico-highlight-enabled-mode))))
 
 
 ;;; Marginalia (n. marginal notes) in the minibuffer.
