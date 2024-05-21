4b0eb758c09ab8a161c1e05ab1aed85439c04259
Bojun Ren <bj.ren.coding@outlook.com>
2024-05-21 20:43:23 +0800

    feat(editor): add configs for `celeste/auto-save`

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -101,6 +101,32 @@
   :config
   (setq whitespace-cleanup-mode-preserve-point t))
 
+;; `auto-save' from manteelazycat (my personal fork).
+(use-package auto-save
+  :init
+  (celeste/prepare-package auto-save)
+
+  (define-minor-mode celeste/auto-save-mode
+    "Save buffers automatically."
+    :global t
+    :lighter " AS"
+    :keymap nil
+    (if celeste/auto-save-mode
+        (auto-save-enable)
+      (auto-save-disable)))
+  (add-hook 'after-init-hook #'celeste/auto-save-mode)
+
+  ;; Disable Emacs's `auto-save-mode'. To check whether this minor mode is
+  ;; enabled in the current buffer, evaluate:
+  ;; (and buffer-auto-save-file-name (>= buffer-saved-size 0))
+  (setq auto-save-default nil)
+  (setq auto-save-silent t)
+
+  :commands (auto-save-enable
+             auto-save-disable)
+  :config
+  ;; `celeste/auto-save-mode' must be re-enabled after changing this value.
+  (setq auto-save-idle 1.0))
 
 
 (provide 'init-editor)
