635521df136ba4d3c732d5caf1bc7d0ae94b1eff
Bojun Ren <bj.ren.coding@outlook.com>
2024-06-04 23:46:23 +0800

    feat(package): tweak use-package and add comments

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -2,6 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
+
+;;; use-package: convenient macro to customize Emacs packages.
+
 (require 'use-package)
 
 ;; Enable `use-package'.
@@ -9,9 +12,18 @@
       use-package-always-defer t ; to accelerate startup
       use-package-expand-minimally t)
 
-;; diminish - change or conceal the mode-line indicator for minor modes.
+;; I feel hard to remember whether to use the plural...
+(defalias 'use-package-handler/:command 'use-package-handler/:commands)
+(add-to-list 'use-package-keywords :command)
+(defalias 'use-package-handler/:function 'use-package-handler/:functions)
+(add-to-list 'use-package-keywords :function)
+
+
+;;; diminish - change or conceal the mode-line indicator for minor modes.
+
 (celeste/require 'diminish)
 
+
 ;;; GNU elpa
 
 ;; Override Emacs's builtin packages here. It should be early enough, in case of
