6369c62a2d7085a78b28074b6f3018e0045e4d7d
Bojun Ren <bj.ren.coding@outlook.com>
2024-07-05 20:10:35 +0800

    fix(sh): edit `auto-mode-alist` in :init section

--- lisp/lang/init-sh.el
+++ lisp/lang/init-sh.el
@@ -4,13 +4,11 @@
 
 (use-package sh-script
   :init
+  (add-to-list 'auto-mode-alist `(,(rx ".zshrc" (* nonl) eos) . sh-mode))
   (ide-zero-define sh
                    :mode sh-mode
                    :linter default)
-  (sh-ide-zero-mode +1)
-  :config
-  (add-to-list 'auto-mode-alist `(,(rx ".zshrc" (* nonl) eos) . sh-mode))
-  )
+  (sh-ide-zero-mode +1))
 
 (provide 'init-sh)
 ;;; init-sh.el ends here
