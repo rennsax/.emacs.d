294065f535b5c2243cba99115f8ebec44662d117
Bojun Ren <bj.ren.coding@outlook.com>
2024-07-23 02:07:38 +0800

    feat(vterm): add `vterm-copy-mode-done-no-exit`
    Now I understand the correct usage of `vterm-copy-mode-done`. So I add
    `vterm-copy-mode-no-exit` for M-w in `vterm-copy-mode`.

--- lisp/init-vterm.el
+++ lisp/init-vterm.el
@@ -78,6 +78,34 @@ open vterm at its parent directory."
   ;; The horizontal margin is useless.
   (add-hook 'vterm-mode-hook (lambda () (setq-local hscroll-margin 0)))
 
+  ;; Fix: in `vterm-copy-mode', "C-w" or "M-w" cannot correctly copy the active
+  ;; region sometimes. Just reuse the logic of `vterm-copy-mode-done' but do not
+  ;; exit `vterm-copy-mode'.
+  ;;
+  ;; Bind this to "C-w" and "M-w".
+  (defun vterm-copy-mode-done-no-exit (arg)
+    "Save the active region or line to the kill ring but do not exit `vterm-copy-mode'.
+
+If a region is defined then that region is killed, with no region then
+current line is killed from start to end.
+
+The option `vterm-copy-exclude-prompt' controls if the prompt
+should be included in a line copy.  Using the universal prefix ARG
+will invert `vterm-copy-exclude-prompt' for that call."
+    (interactive "P")
+    (unless vterm-copy-mode
+      (user-error "This command is effective only in vterm-copy-mode"))
+    (unless (use-region-p)
+      (goto-char (vterm--get-beginning-of-line))
+      ;; Are we excluding the prompt?
+      (if (or (and vterm-copy-exclude-prompt (not arg))
+              (and (not vterm-copy-exclude-prompt) arg))
+          (goto-char (max (or (vterm--get-prompt-point) 0)
+                          (vterm--get-beginning-of-line))))
+      (set-mark (point))
+      (goto-char (vterm--get-end-of-line)))
+    (kill-ring-save (region-beginning) (region-end)))
+
   (bind-keys :map vterm-mode-map
              ("C-u" . vterm--self-insert)
              ("C-x C-e" . (lambda () (interactive)
@@ -85,9 +113,12 @@ open vterm at its parent directory."
                             (vterm-send-key "e" nil nil 'ctrl)))
              ("C-q" . vterm-send-next-key)
              :map vterm-copy-mode-map
-             ("q" . vterm-copy-mode-done)
+             ;; RET: `vterm-copy-mode-done', which will copy the region or the current line.
+             ("q" . vterm-copy-mode)
              ("p" . previous-line)
-             ("n" . next-line))
+             ("n" . next-line)
+             ("M-w" . vterm-copy-mode-done-no-exit)
+             ("C-w" . vterm-copy-mode-done-no-exit))
 
   ;; It's true that I'm a Zsh lover.
   (setq vterm-tramp-shells '(("docker" "/bin/sh")
