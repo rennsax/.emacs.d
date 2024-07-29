b362b1ff244d9a30d7937b60439d1c3b06265123
Bojun Ren <bj.ren.coding@outlook.com>
2024-07-29 17:25:01 +0800

    chore(nix): add Nix-managed Emacs modules to load-path

--- lisp/lang/init-nix.el
+++ lisp/lang/init-nix.el
@@ -10,6 +10,14 @@
                    :lsp ("nil"))
   )
 
+(when (or (executable-find "darwin-rebuild")
+          (executable-find "nixos-rebuild")
+          (executable-find "home-manager"))
+  (let ((module-path (expand-file-name "~/.config/emacs-modules/lib")))
+    (if (file-directory-p module-path)
+        (add-to-list 'load-path module-path)
+      (warn "You should build emacs-modules!"))))
+
 (provide 'init-nix)
 ;;; init-nix.el ends here
 
