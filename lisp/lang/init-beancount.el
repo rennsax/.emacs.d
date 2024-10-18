9c35230b0ad4df9953d63e0de88e371d68f20cf7
Bojun Ren <bj.ren.coding@outlook.com>
2024-10-18 09:56:05 +0800

    beancount: enable abbrev-mode

--- lisp/lang/init-beancount.el
+++ lisp/lang/init-beancount.el
@@ -8,6 +8,7 @@
   :mode (("\\.beancount\\'" . beancount-mode)
          ("\\.bean\\'" . beancount-mode))
   :config
+  (add-hook 'beancount-mode-hook #'abbrev-mode)
   (setq beancount-transaction-indent 4)
   (setq beancount-use-ido nil)
   (setq beancount-number-alignment-column 0))
