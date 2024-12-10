2ee44040369640dd99778fbadb4cdc51004bd150
Bojun Ren <bj.ren.coding@outlook.com>
2024-12-10 16:53:11 +0800

    utils!: remove `keyboard-quit-dwim`
    Use the native `keyboard-quit` instead, because I did not find any benefits of
    this customized one.

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -172,30 +172,6 @@ If FORCE-P, delete without confirmation."
     (kill-local-variable 'mode-line-format)
     (force-mode-line-update)))
 
-(defun keyboard-quit-dwim ()
-  "Do-What-I-Mean behaviour for a general `keyboard-quit'.
-
-The generic `keyboard-quit' does not do the expected thing when
-the minibuffer is open.  Whereas we want it to close the
-minibuffer, even without explicitly focusing it.
-
-The DWIM behaviour of this command is as follows:
-
-- When the region is active, disable it.
-- When a minibuffer is open, but not focused, close the minibuffer.
-- When the Completions buffer is selected, close it.
-- In every other case use the regular `keyboard-quit'."
-  (interactive)
-  (cond
-   ((region-active-p)
-    (keyboard-quit))
-   ((derived-mode-p 'completion-list-mode)
-    (delete-completion-window))
-   ((> (minibuffer-depth) 0)
-    (abort-recursive-edit))
-   (t
-    (keyboard-quit))))
-
 
 
 ;;; unfill-region
@@ -238,7 +214,6 @@ useful, e.g., for use with `visual-line-mode'."
 
 (keymap-global-set "s-r" #'reload-this-file)
 (keymap-global-set "<f9>" #'spawn-sub-emacs)
-(keymap-global-set "<remap> <keyboard-quit>" #'keyboard-quit-dwim)
 (keymap-global-set "M-u" #'upcase-previous-word)
 (keymap-global-set "<remap> <keyboard-escape-quit>" #'keyboard-escape-quit-no-close-win)
 
