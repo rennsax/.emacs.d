3343bcda80f0612b1ceb9986dd1b536f8af7c768
Bojun Ren <me.rennsax@gmail.com>
2025-08-21 14:37:21 +0800

    cc: add style definitions

--- lisp/lang/init-cc.el
+++ lisp/lang/init-cc.el
@@ -7,10 +7,44 @@
   :mode (("[Mm]akefile\\'" . makefile-gmake-mode)))
 
 (use-package cc-mode
-  :init
+  :mode ("\\.h\\'" . c++-mode)
   :config
-  (setq c-default-style "k&r"
-        c-basic-offset 4)
+
+  ;; NOTE: see `c-fallback-style' for fallback styles.
+  (defconst celeste/cc-style
+    '((c-basic-offset . 4)
+      (c-comment-only-line-offset . 0)
+      (c-hanging-braces-alist . (
+                                 (brace-list-open)
+                                 (inline-open after)
+                                 (class-open after)
+                                 (class-close)
+                                 (defun-open after)
+                                 ;;  (brace-entry-open)
+                                 ;;  (statement-cont)
+                                 ;;  (substatement-open after)
+                                 ;;  (block-close . c-snug-do-while)
+                                 ;;  (extern-lang-open after)
+                                 (namespace-open after)
+                                 ;;  (module-open after)
+                                 ;;  (composition-open after)
+                                 ;;  (inexpr-class-open after)
+                                 ;;  (inexpr-class-close before)
+                                 ;; (arglist-cont-nonempty)
+                                 ))
+      (c-offsets-alist . ((inline-open . 0)
+                          (label . 0)
+                          (innamespace . 0)
+                          (inextern-lang . 0)))))
+  (c-add-style "Celeste" celeste/cc-style)
+
+  (celeste/add-mode-hook '(c-mode c++-mode)
+      (defun celeste/cc-set-style ()
+        (c-set-style "Celeste")
+        (setq tab-width 4
+              indent-tabs-mode nil)
+        ;; (c-toggle-auto-newline +1)
+        ))
 
   (celeste/setup-lang cc
     :modes (c-mode c-ts-mode c++-mode c++-ts-mode)
